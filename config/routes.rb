975724057816b871aec54eadd02ed773ad6443ec
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-07-27 07:36:10 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get "static_pages/about"
   root "application#hello"
 end
