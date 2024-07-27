6531fd8edc18c58c301e48691a99068aef9cdc76
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-07-27 09:34:58 +0900

    api追加

--- Gemfile
+++ Gemfile
@@ -47,8 +47,9 @@ gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-
+# 以下追加
 gem 'ridgepole'
+gem 'active_model_serializers'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get "static_pages/home"
+  get "static_pages/help"
   get "static_pages/about"
   get "static_pages/no_title"
+
+  namespace :api do
+    namespace :v1 do
+      get "static_pages/home"
+    end
+  end
+
+  namespace :test do
+    get "test/index"
+  end
 end
