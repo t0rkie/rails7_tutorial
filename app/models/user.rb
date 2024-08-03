b774dbbf7051d8df8aa3b9da25f1c172eb65811e
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-08-03 10:48:57 +0900

    第６章終わり

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  # self.email = self.email.downcaseと等価 右式のselfは省略できる
+  before_save { self.email = email.downcase }
 
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  # password_digestカラム追加後、bcryptで使用可能に
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- apply_schema.sh
+++ apply_schema.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker-compose exec web ridgepole --apply -f  db/schemas/Schemafile  -E development  --config config/database.yml
+docker-compose exec web ridgepole --apply -f  db/schemas/Schemafile  -E test  --config config/database.yml
