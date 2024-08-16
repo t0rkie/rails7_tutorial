d1cae66acf7e455c98d19e6fec72930743837439
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-08-16 23:28:24 +0900

    第８章完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    # User.new(name: "Foo Bar", email: "foo@invalid", password: "foo", password_confirmation: "bar") と同義
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the sample app!"
+      # redirect_to user_url(@user)と同義
+      redirect_to @user
+    else
+      # status: unprocessable_entity => HTTPステータスコード 422 Unprocessable Entity
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    # User.new(params[user_params])でパラメータを直接newするのではなく、フィルターをかける
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
